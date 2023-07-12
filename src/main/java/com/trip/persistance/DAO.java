b245641d43794f34825ff69f511545fb6b14f331
Taejun33312 <a01055384863@gmail.com>
2023-07-12 17:29:20 +0900

    07. 12. 프로젝트 생성

---
+++ src/main/java/com/trip/persistance/DAO.java
@@ -0,0 +1,5 @@
+package com.trip.persistance;
+
+public class DAO {
+
+}
---
+++ src/main/java/com/trip/utils/TripFilter.java
@@ -0,0 +1,37 @@
+package com.trip.utils;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = "/*")
+public class TripFilter implements Filter {
+
+	/**
+	 * doFilter 메소드에서 본인이 필요한 동작 (모든 요청에 응답해야하는 경우) 구현하면 됩니다.
+	 * 단, 공용 파일이므로 작업 필요 시 사전에 알리기 바랍니다.
+	 * @author bcdc124
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// 한글처리
+		request.setCharacterEncoding("UTF-8");
+		// 한글처리
+
+		chain.doFilter(request, response);
+	}
+	
+	// 사용하는 부분 아님
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	@Override
+	public void destroy() {}
+	// 사용하는 부분 아님
+}
